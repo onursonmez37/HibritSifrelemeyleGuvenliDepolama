a8838491781d0214c46a334189bd23950cbff0b1
onursonmez37 <onursonmez37@users.noreply.github.com>
2022-12-19 19:43:06 +0300

    projem
    _

---
+++ hibritSifrelemeBilgisayardaGüvenliDepolama/src/hibritSifrelemeBilgisayardaGüvenliDepolama/password.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibritSifrelemeBilgisayardaGüvenliDepolama;
+
+/**
+ *
+ * @author basde
+ */
+public class password {
+    public String Password;
+    //constructor
+    password() {
+    }
+    //constructor
+    password(String password) {
+        //Password = password;
+        System.out.println(password);
+    }
+
+}
